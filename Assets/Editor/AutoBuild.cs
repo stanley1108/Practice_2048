3fd39cf0990cb884fa6588e4df615afa47183db7
stanley <stanley.cra@gmail.com>
2014-12-25 23:22:23 +0800

    +Change AutoBuild path

--- Assets/Editor/AutoBuild.cs
+++ Assets/Editor/AutoBuild.cs
@@ -5,6 +5,18 @@ class AutoBuild
 	static void PerformBuild ()
 	{
 		string[] scenes = { "Assets/Scene/Gameplay.unity" };
-		BuildPipeline.BuildPlayer(scenes, "my2048.app", BuildTarget.StandaloneOSXIntel, BuildOptions.None);
+		BuildPipeline.BuildPlayer(GetScenePaths(), "my2048.app", BuildTarget.StandaloneOSXIntel, BuildOptions.None);
+	}
+
+	static string[] GetScenePaths()
+	{
+		string[] scenes = new string[EditorBuildSettings.scenes.Length];
+		
+		for(int i = 0; i < scenes.Length; i++)
+		{
+			scenes[i] = EditorBuildSettings.scenes[i].path;
+		}
+		
+		return scenes;
 	}
 }
