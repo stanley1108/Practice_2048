a751a70d632f6f60d25890b808f78e9c8123f3d8
stanley <stanley.cra@gmail.com>
2014-12-25 23:58:41 +0800

    +clear code

--- Assets/Editor/AutoBuild.cs
+++ Assets/Editor/AutoBuild.cs
@@ -6,18 +6,6 @@ class AutoBuild:MonoBehaviour
 	static void PerformBuild ()
 	{
 		string[] scenes = { "Assets/Scene/Gameplay.unity" };
-		BuildPipeline.BuildPlayer(GetScenePaths(), Application.dataPath + "/my2048.app", BuildTarget.StandaloneOSXIntel, BuildOptions.None);
-	}
-
-	static string[] GetScenePaths()
-	{
-		string[] scenes = new string[EditorBuildSettings.scenes.Length];
-		
-		for(int i = 0; i < scenes.Length; i++)
-		{
-			scenes[i] = EditorBuildSettings.scenes[i].path;
-		}
-		
-		return scenes;
+		BuildPipeline.BuildPlayer(scenes, Application.dataPath + "/my2048.app", BuildTarget.StandaloneOSXIntel, BuildOptions.None);
 	}
 }
