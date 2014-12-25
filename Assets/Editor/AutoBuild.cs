b23563f32a3626f3c41600d13dd7adcf0dd4273f
stanley <stanley.cra@gmail.com>
2014-12-26 00:09:08 +0800

    +test again...

--- Assets/Editor/AutoBuild.cs
+++ Assets/Editor/AutoBuild.cs
@@ -6,6 +6,6 @@ class AutoBuild:MonoBehaviour
 	static void PerformBuild ()
 	{
 		string[] scenes = { "Assets/Scene/Gameplay.unity" };
-		BuildPipeline.BuildPlayer(scenes, "/Build/my2048.app", BuildTarget.StandaloneOSXIntel, BuildOptions.None);
+		BuildPipeline.BuildPlayer(scenes, "Build/my2048.app", BuildTarget.StandaloneOSXIntel, BuildOptions.None);
 	}
 }
