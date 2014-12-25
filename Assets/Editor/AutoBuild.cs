339686249527bf59e815b3c51db1d50d82de8ca6
stanley <stanley.cra@gmail.com>
2014-12-25 22:39:16 +0800

    +Add AutoBuild script

---
+++ Assets/Editor/AutoBuild.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+class AutoBuild
+{
+	static void PerformBuild ()
+	{
+		string[] scenes = { "Assets/Gameplay.unity" };
+		BuildPipeline.BuildPlayer(scenes, "my2048.app", BuildTarget.StandaloneOSXIntel, BuildOptions.None);
+	}
+}
